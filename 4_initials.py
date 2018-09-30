3542c29a43351cfb0c08bbcdb86af556216a4ae0
William Kurzius <bill@kurzius.net>
2018-09-30 10:26:20 -0400

    Completed sample code for 3 and 4

---
+++ 4_initials.py
@@ -0,0 +1,33 @@
+import turtle
+
+t = turtle.Turtle()
+
+## begin W
+
+t.right(90)
+t.forward(100)
+t.left(135)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.left(135)
+t.forward(100)
+
+t.penup()
+t.back(100)
+t.right(90)
+t.forward(25)
+t.left(90)
+t.pendown()
+
+## begin K
+
+t.forward(100)
+t.back(50)
+t.right(45)
+t.forward(70)
+t.back(70)
+t.right(90)
+t.forward(70)
+
+turtle.mainloop()
